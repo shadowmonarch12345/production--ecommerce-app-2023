59e8d366ca25dffbb0ec43c98086267eb5db748a
shadowmonarch12345 <138355840+shadowmonarch12345@users.noreply.github.com>
2023-08-11 20:34:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ dotenv.config();
 connectDB();
 
 //rest object
-const__filename=fileURLToPath(import.meta.url);
-const__dirname=path.dirname(__filename);
+//const__filename = fileURLToPath(import.meta.url);
+//const__dirname=path.dirname(__filename);
 const app = express();
 
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')));
+app.use(express.static(path.join(__dirname,"./client/build")));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
@@ -32,7 +32,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
 })
 //PORT
 const PORT = process.env.PORT || 8080;
